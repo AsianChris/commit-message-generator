c13acab089844f47fbe72f4c4acfdfa120d81446
Chris Baptista <chris.baptista@me.com>
2016-04-07 21:44:00 -0400

    Why so serious?

--- app/Model/Message.php
+++ app/Model/Message.php
@@ -32,7 +32,7 @@ class Message
    *
    * @param string $message
    */
-  public function __construct(string $message) {
+  public function __construct(\string $message) {
     $this->setMessage($message);
   }
 
@@ -41,7 +41,7 @@ class Message
    *
    * @param string $message Commit Message
    */
-  public function setMessage(string $message) {
+  public function setMessage(\string $message) {
     $this->message = $message;
 
     $this->setHash();
